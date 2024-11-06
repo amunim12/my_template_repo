c73c61c71a9be887ee6ba0a77c9b94e5308a7974
Abdul Munim <abdulmunim.personal@gmail.com>
2024-11-06 23:53:09 +0500

    logger and exception file edited

--- src/exceptions/exception.py
+++ src/exceptions/exception.py
@@ -1,20 +1,13 @@
 import sys
-from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
-    if error_detail is not None:
-        _,_,exc_tb=error_detail.exc_info()
-        filename = exc_tb.tb_frame.f_code.co_filename
-        error_message = "Error occured in python script name [{0}] at line number [{1}] error message [{2}]".format(
-        filename, exc_tb.tb_lineno, str(error))
-    else:
-        error_message = str(error)
-    return error_message
+class CustomException(Exception):
+    def __init__ (self, error_message, error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
 
-class customException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return self.error_message
+        return f"Error occured in the python script name {self.file_name} line number {self.lineno} error message{self.error_message}" 
+    
